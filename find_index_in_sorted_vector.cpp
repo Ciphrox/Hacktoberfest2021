2dfdaff301c3d07595b057517bf619f587fb3886
Ronak-Jethava <ronakjethava1511@gmail.com>
2021-10-05 22:58:31 +0530

    Bug fixed.

--- find_index_in_sorted_vector.cpp
+++ find_index_in_sorted_vector.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 // return index of a number x
 // if x is not present in a vector, return -1
-bool find_index(vector<int> &A, int x){
+int find_index(vector<int> &A, int x){
     int l = 0, r = A.size()-1;
     while(l < r){
         int mid = (l+r)/2;
@@ -21,14 +21,15 @@ bool find_index(vector<int> &A, int x){
 int main(){
     vector<int> v{1,2,3,4,5,6,7,8,9,10};
     
-    if(find_index(v, 4))
-        cout << "4 is present in vector v.\n";
+    int idx;
+    if((idx = find_index(v, 4)) != -1)
+        cout << "4 is present in vector v at index " << idx << endl;
     else
         cout << "4 is not present in vector v.\n";
 
     
-    if(find_index(v, 30))
-        cout << "30 is present in vector v.\n";
+    if((idx = find_index(v, 30)) != -1)
+        cout << "30 is present in vector v at index " << idx << endl;
     else
         cout << "30 is not present in vector v.\n";
 }
