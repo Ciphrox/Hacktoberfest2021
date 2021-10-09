c42f00bd295ba1ec62875955545191ab2308be30
Malik Muzzammil <mzmlrafiqcse@gmail.com>
2021-10-10 00:32:51 +0530

    made folders

---
+++ C++ & C/10.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+const double pi=3.14;
+const char newline='\n';
+int main(){
+ double r=5.0;
+ double circle;
+ circle=2*pi*r;
+ cout<<circle;
+ cout<<newline;
+
+
+
+
+}
---
+++ C++ & C/5.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main() {
+ char str[]="hello cout";
+ cout<<"value of str is = "<<str<<endl;
+ return 0;
+ }
