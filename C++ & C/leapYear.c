761f88ed7b932efee24fbc07414ad26303854baf
Rishi-2211 <91396134+Rishi-2211@users.noreply.github.com>
2021-10-14 22:20:24 +0530

    Update leapYear.c

--- C++ & C/leapYear.c
+++ C++ & C/leapYear.c
@@ -3,11 +3,17 @@
 int main()
 {
     int year;
+    
     printf("Enter a year: ");
+    
     scanf("%d",&year);
+    
     if (( year%400 == 0) || (( year%4 == 0 ) && ( year%100 != 0)))
     printf("Leap year");
+    
     else
+        
     printf("Not a Leap Year");
+    
     return 0;
 }
