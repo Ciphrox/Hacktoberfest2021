10a22250861feda67a91c902647e0e33c36ec754
TheHanyu <91917207+TheHanyu@users.noreply.github.com>
2021-10-16 16:00:58 -0700

    password-generator.py (#302)
    * Create python.password.generator
    
    * Create password-generator.py
    
    * Delete python.password.generator
    
    * Rename password-generator.py to Python/password-generator.py
    
    Co-authored-by: Dark Coder Cat | Vansh <darkcodercat@gmail.com>

---
+++ Python/password-generator.py
@@ -0,0 +1,10 @@
+print("Hi, are you having trouble making a password?")
+print("let me help you!")
+number = input("give me a number from 1-3")
+password = number
+animal = input("do you prefer dogs or cats?")
+password = number + animal
+color=input("what is your favorite color?")
+password = color+number+animal
+book=input("ok, last question. What is your favorite book?(the longer the title, the longer the password!")
+password=book+password
