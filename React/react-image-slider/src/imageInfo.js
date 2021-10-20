8af93e73711bab58c8f1fb97bdea3768e5076b27
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-10-20 11:26:11 +0530

    CREATE: react-image-slider added

---
+++ React/react-image-slider/src/imageInfo.js
@@ -0,0 +1,22 @@
+export const imageInfo = [
+  {
+    imageSrc:
+      "https://images.unsplash.com/photo-1615394687989-2e889e8ac363?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+  },
+  {
+    imageSrc:
+      "https://images.unsplash.com/photo-1615394767355-b2a71c95468c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80",
+  },
+  {
+    imageSrc:
+      "https://images.unsplash.com/photo-1615394740748-98242eca48bd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1350&q=80",
+  },
+  {
+    imageSrc:
+      "https://images.unsplash.com/photo-1615394695852-da39a8df9bf1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+  },
+  {
+    imageSrc:
+      "https://images.unsplash.com/photo-1615394718108-699bad3979a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1351&q=80",
+  },
+];
