d1618666d780e90a2046cc5c13ccdab1a1bd5888
Victor Loveday <56683410+Vicviral@users.noreply.github.com>
2021-10-21 15:42:24 +0100

    Feat: String

--- Kotlin/KotlinStandardLib.kt
+++ Kotlin/KotlinStandardLib.kt
@@ -67,6 +67,8 @@ fun main() {
     print(twoElements.second) //Output: 300.00
 
 
+
+
     //    Destructuring Declarations:
 //    Destructuring declaration is the easiest way to create variables &
 //    initialize all of them in a single line of code:
@@ -81,4 +83,78 @@ fun main() {
     for ((key, value) in map) print("Id: $key, Name: $value ,")
     //Output: Id: 1, Name: Harry, Id: 2, Name: Hermione, Id: 3, Name: Ron
 
+
+
+
+    //String:
+//    Daily we deal with many functionalities where it's required to manipulate
+//    the strings. Earlier to avail various String functions in Android,
+//    we had no option other than integrating the Apache Common library,
+//    as Android's TextUtils class doesn't provide many functionalities. However,
+//    Kotlin offers a variety of functions that helps to tweak and trick the output as per the requirement.
+//    Let's take a look at a few of Kotlin's in-built useful
+//    methods that save us from writing lengthy logics to achieve a requirement:
+
+    "Ja49va1K7otl54in7".partition { "123456789".contains(it)}
+    //Output: (4917547, JavaKotlin)
+
+    "L7ea3rn 1Kotl8in 12".filter { it.isDigit() }
+    //Output: 731812
+
+    "L7ea3rn 1Kotl8in 12F".findLast { it.isDigit() }
+    //Output:2
+    //Similar methods: findAnyOf(), findLastAnyOf()
+
+    "Learn Kotlin".all { it.isLowerCase() }
+    //Output: false
+
+    "Learn Kotlin".any { it.isLowerCase() }
+    //Output: true
+
+    "Learn Kotlin".dropLastWhile{it.isLowerCase()}
+    //Output; Learn K
+    //Similar methods: dropWhile{}, drop(), dropLast()
+
+    "Learn Kotlin".associateWith { it.toUpperCase() }
+    //Output: {L=L, e=E, a=A, r=R, n=N,  = , K=K, o=O, t=T, l=L, i=I}
+    //Similar methods: associate{}, associateBy{}, associateByTo(), associateWithTo()
+
+    "Line One\n Line Two \n Line Three".lines()
+    //Output: [Line One,  Line Two ,  Line Three]
+
+    "Learn Kotlin".replaceAfter(' ', "Java")
+    //Output: Learn Java
+    //Similar methods: replace(), replaceAfterLast(), replaceBefore(), replaceBeforeLast(), replaceFirst(), replaceIndentByMargin(), replaceRange(), replaceFirst()
+
+    "Learn Kotlin".substringAfter("a")
+    //Output: rn Kotlin
+    //Similar methods: substring(), substringAfterLast(), substringBefore(), substringBeforeLast()
+
+    "Android_App_Java".commonPrefixWith("Android_App_Kotlin")
+    //Output: Android_App_
+    //Similar methods: commonSuffixWith()
+
+    "Learn Kotlin".removeSurrounding("L", "lin")
+    //Output: earn Kot
+    //Similar methods: removePrefix(), removeSuffix(), removeRange()
+
+    "\t".isBlank()
+    //Output: true
+    //Similar methods: ifBlank{}, isNotBlank(), isNotBlank(), isNullOrBlank()
+
+    "\t".isEmpty()
+    //Output: false
+    //Similar methods: ifEmpty{}, isNotEmpty(), isNullOrEmpty(), orEmpty()
+
+    "Learning\tKotlin".trim('L','B', 'n')
+    //Output: earning	Kotli
+    //Similar methods: trimIndent(), trimMargin(), trimStart(), trimEnd(), trimStart('L'), trimEnd('n')
+
+    "Learning Kotlin".padStart(20, '.')
+    //Output: .....Learning Kotlin
+    //Similar method: padEnd()
+
+    "learning kotlin".capitalize()
+    //Output: Learning kotlin
+    //Similar method: decapitalize()
 }
