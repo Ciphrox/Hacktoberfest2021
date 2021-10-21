5c9cc03056204742e3f3297702919202a1eb768d
Victor Loveday <56683410+Vicviral@users.noreply.github.com>
2021-10-21 15:30:31 +0100

    Feat: Destructuring & Declarations

--- Kotlin/KotlinStandardLib.kt
+++ Kotlin/KotlinStandardLib.kt
@@ -49,7 +49,6 @@ fun main() {
 //    No, not at all! But, with the help of `Pair` and `Triple` classes,
 //    this isn't a pain anymore. We can have a method that can return up to three elements:
 
-
     fun getThreeElements(): Triple<String, Double, String> {
         return Triple("Learn Kotlin", 200.00, "Educational")
     }
@@ -67,5 +66,19 @@ fun main() {
     print(twoElements.first)  //Output: Learn Java
     print(twoElements.second) //Output: 300.00
 
+
+    //    Destructuring Declarations:
+//    Destructuring declaration is the easiest way to create variables &
+//    initialize all of them in a single line of code:
+
+    val (id, name, age) = Person(101, "Harry Potter", 15)
+    //Output: id = 101, name = Harry Potter, age = 15
+
+    val (x, y, z) = arrayOf(1.3, 2.4, 3.8)
+    //Output: x= 1.3, y= 2.4, z= 3.8
+
+    val map = mutableMapOf(1 to "Harry", 2 to "Hermione", 3 to "Ron")
+    for ((key, value) in map) print("Id: $key, Name: $value ,")
+    //Output: Id: 1, Name: Harry, Id: 2, Name: Hermione, Id: 3, Name: Ron
 
 }
