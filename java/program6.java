8e1241381df8c626b08321495adab66c3f357902
AmanAkr18 <60777384+AmanAkr18@users.noreply.github.com>
2021-10-22 06:38:15 +0530

    Unique digit in a number (#427)
    * Program to find unique digits in a number
    
    * program to find unique digit in a number

---
+++ java/program6.java
@@ -0,0 +1,22 @@
+package practice;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class program6 {
+    static int unique(int n){
+        Set<Integer> number= new HashSet<Integer>();
+        while(n>0){
+            number.add(n%10);
+            n/=10;
+        }
+        return number.size();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("Unique Number " + unique(a));
+    }
+}
