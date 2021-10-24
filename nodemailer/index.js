c620b36a5ea7bd5829196122e393a166f13f195a
Harish Jartarghar <51698550+harishjartarghar@users.noreply.github.com>
2021-10-25 03:16:12 +0530

    nodemailer added (#457)

---
+++ nodemailer/index.js
@@ -0,0 +1,25 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'youremail@gmail.com',
+    pass: 'yourpassword'
+  }
+});
+
+var mailOptions = {
+  from: 'youremail@gmail.com',
+  to: 'myfriend@yahoo.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!' //html:'<p>Hello</p>'
+ 
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
