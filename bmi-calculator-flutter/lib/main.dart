68350df1aff0a9494bb9b770bc529f2995d252f0
Ijas Hussain <lprdeo@gmail.com>
2021-10-28 17:15:17 +0530

    Removed Debug Banner

--- bmi-calculator-flutter/lib/main.dart
+++ bmi-calculator-flutter/lib/main.dart
@@ -7,6 +7,7 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: InputPage(),
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: Color(0xFF0b1034),
